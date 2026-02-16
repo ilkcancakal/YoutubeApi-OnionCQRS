5fb504ccf1c26fe31176996a7d03dfe0914bb237
ilkcancakal <ilkcan.cakal@gmail.com>
2026-02-16 23:44:10 +0300

    Add project files.

---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail()
+        {
+
+        }
+        public Detail(string title, string description, int categoryId)
+        {
+            categoryId = categoryId;
+            Title = title;
+            Description = description;
+        }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
